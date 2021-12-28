08804dec98d689a949b7d4960d17d6d4dc1f7c04
Akshat Toolaj Sinha <2020csb1068@iitrpr.ac.in>
2021-12-28 17:39:41 +0530

    line_bug_fixed & proper indentation

--- popup.js
+++ popup.js
@@ -1,44 +1,41 @@
 document.getElementById("submitbutton").addEventListener("click", taskPutter);
 document.getElementById("resetbutton").addEventListener("click", taskremover);
 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
   fetchItem();
-})
+});
 
-function fetchItem(){
-  let num=parseInt(localStorage.getItem('number'));
+function fetchItem() {
+  let num = parseInt(localStorage.getItem("number"));
   console.log(num);
-  for( let key=1;key<=num;key++){
-      let text=localStorage.getItem(key);
-      console.log('Value currently is ' + text);
-      let task = document.createElement("p");
-      task.appendChild(document.createTextNode(key + ". " + text));
-      document.getElementById("rem_task").appendChild(task);
+  for (let key = 1; key <= num; key++) {
+    let text = localStorage.getItem(key);
+    var new_text = str.replace(/.{6}/g, "$0\n");
+    let task = document.createElement("p");
+    task.appendChild(document.createTextNode(key + ". " + new_text));
+    document.getElementById("rem_task").appendChild(task);
   }
 }
 function taskPutter() {
   let text = document.getElementById("text").value;
   if (text == "") return;
 
-  let num=parseInt(localStorage.getItem('number'));
-  if(num==NaN){
-    num=0;
+  let num = parseInt(localStorage.getItem("number"));
+  if (num == NaN) {
+    num = 0;
   }
-  console.log(num);
   num += 1;
-  localStorage.setItem('number',num);
+  localStorage.setItem("number", num);
   let task = document.createElement("p");
   task.appendChild(document.createTextNode(num + ". " + text));
   document.getElementById("rem_task").appendChild(task);
   document.getElementById("text").value = "";
-  localStorage.setItem(num,text);
+  localStorage.setItem(num, text);
 }
 
 function taskremover() {
-  let num=parseInt(localStorage.getItem('number'));
-  console.log(num);
+  let num = parseInt(localStorage.getItem("number"));
   document.getElementById("rem_task").innerHTML = "";
-  console.log("To-do list is removed");
-  num=0;
-  localStorage.setItem('number',num);
+  num = 0;
+  localStorage.setItem("number", num);
 }
